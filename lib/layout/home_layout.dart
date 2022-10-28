7579012ac1db257cbbe6bc17e86f5ea69eb801eb
YossefHussein <youssef.hussein.business@gmail.com>
2022-10-28 17:44:30 +0200

    update

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -6,6 +6,7 @@ import '../shared/widget/appbar_method/appBar.dart';
 class HomeLayout extends StatelessWidget {
   const HomeLayout({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
