3ef2ec3c6ce8b5f794c3fdf174f07210b968b89c
YossefHussein <youssef.hussein.business@gmail.com>
2024-01-18 18:31:26 +0200

    complete the feature

--- lib/modules/4_result.dart
+++ lib/modules/4_result.dart
@@ -97,15 +97,17 @@ class ResultScreen extends StatelessWidget {
                       ),
                     ),
                     GestureDetector(
-                        onTap: () async {
-                            final Uri _url = Uri.parse(
-                              'https://ourworldindata.org/obesity#',
-                            );
-                            if (!await launchUrl(_url)) {
-                              throw Exception('Could not launch $_url');
-                            }
-                        },
-                        child: Text("to see artical click on photo"))
+                      onTap: () async {
+                        final Uri _url = Uri.parse(
+                          'https://ourworldindata.org/obesity#',
+                        );
+                        if (!await launchUrl(_url,
+                            mode: LaunchMode.externalApplication)) {
+                          throw Exception('Could not launch $_url');
+                        }
+                      },
+                      child: Text("to see artical click on photo"),
+                    )
                   ],
                 ),
               ),
